852de5bd7f4676526a31cbf6f730417115bf935f
Anatoly Pruchay <anatoly.pruchay@gmail.com>
2023-03-11 12:45:37 +0300

    TSKMGR-2: add linaria, material-ui, icons & font (#2)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import linaria from '@linaria/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), linaria()],
+  resolve: {
+    alias: {
+      '@mui/styled-engine': '@mui/styled-engine-sc'
+    },
+  },
 })
