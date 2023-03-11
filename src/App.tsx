07817cac6b98fe77caee3bc11c0619c76963eb8d
Anatoly Pruchay <anatoly.pruchay@gmail.com>
2023-03-11 14:12:42 +0300

    TSKMGR-3: add basic Layout (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { Layout } from './components';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <Layout>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -27,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </div>
+    </Layout>
   )
 }
 
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Container, Box } from '@mui/material';
+import { css, cx } from '@linaria/core';
+
+interface Props {
+  children: React.ReactNode
+}
+
+export const Layout = ({ children }: Props) => {
+  return (
+    <Container maxWidth="xl" className={cx(containerStyles, borderStyles)}>
+      <Box className={cx(boxStyles, borderStyles)}>
+        { children }
+      </Box>
+    </Container>
+  )
+}
+
+const containerStyles = css`
+  && {
+    height: 100vh;
+    width: 1280px;
+    padding: 5px;
+  }
+`;
+
+const borderStyles = css`
+  border-radius: 20px;
+`;
+
+const boxStyles = css`
+  width: 100%;
+  height: calc(100vh - 10px);
+  background-color: #F3F9FE;
+`;
